d9d86178479e17bee853931cb6a00bb77f482b7d
Leebumju <dlqkawns@naver.com>
2022-02-27 21:57:39 +0900

    화면 구성

---
+++ SignUp/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  SignUp
+//
+//  Created by 이범준 on 2022/02/27.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissModal() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
